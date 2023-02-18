974df3086ef661c663e3671f4561b94f7621177f
ebor94 <ebor94@hotmail.com>
2023-02-18 17:59:30 -0500

    Ajustando headers permitidos

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Allow", "GET, POST");
   next();
 });
 /*await readFile();*/
