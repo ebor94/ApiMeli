4efbe546f1a7b17867422ced0339d34c583b093d
ebor94 <ebor94@hotmail.com>
2023-02-18 17:52:55 -0500

    renombrando variables acordes

--- controllers/saveData.js
+++ controllers/saveData.js
@@ -1,35 +1,34 @@
 const serviceGetItem = require("../service/service");
 const fs = require("fs"); // filesystem
 const config = require("../config/file");
-let dataz = [];
-const Filename = "technical_challenge_data.csv";
-let data = [];
+let item = [];
+let lines = [];
 
 async function fileRead(req, res) {
   const readStream = fs.createReadStream(
     __dirname + "/" + `${config.file.name}` + "." + `${config.file.extension}`
   );
-  readStream.on("data", (chunk) => (data += chunk));
+  readStream.on("data", (chunk) => (lines += chunk));
   readStream.on("end", async () => {
-    await readCsv(data);
+    await readCsv(lines);
     res.json({ mensaje: "datos almacenados" });
   });
 }
 
-async function readCsv(dat) {
-  dat
+async function readCsv(fila) {
+  fila
     .split("\r\n")
     .map((line) => line.split(`${config.file.delimiter}`))
-    .map(function (datay, i) {
+    .map(function (items, i) {
       if (i > 0)
-        return dataz.push({
-          item: datay[0] + datay[1],
-          pais: datay[0],
-          id: datay[1],
+        return item.push({
+          item: items[0] + items[1],
+          pais: items[0],
+          id: items[1],
         });
     });
 
-  await serviceGetItem(dataz);
+  await serviceGetItem(item);
 
   return;
 }
